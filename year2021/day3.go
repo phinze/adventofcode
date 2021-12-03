bb2056bb769fda74724caee7a9b7aa737a1add0b
Paul Hinze <phinze@phinze.com>
2021-12-03 13:01:15 -0600

    working on part 2... but something is wrong...

--- year2021/day3.go
+++ year2021/day3.go
@@ -18,6 +18,11 @@ type DayThreeOutput struct {
 	EpsilonRate uint64
 
 	PartOneAnswer int
+
+	OxygenRating   uint64
+	ScrubberRating uint64
+
+	PartTwoAnswer int
 }
 
 func parseDayThree(rawInput string) (*DayThreeInput, error) {
@@ -72,6 +77,44 @@ func solveDayThree(in *DayThreeInput) (*DayThreeOutput, error) {
 	log.Printf("not gamma %b", (^out.GammaRate & (uint64(math.Pow(2, float64(in.BitLength))) - 1)))
 	out.EpsilonRate = ^out.GammaRate & (uint64(math.Pow(2, float64(in.BitLength))) - 1)
 	out.PartOneAnswer = int(out.GammaRate * out.EpsilonRate)
+
+	candidateOxygenRatings := make([]uint64, len(in.Diagnostics))
+	candidateCO2Ratings := make([]uint64, len(in.Diagnostics))
+	copy(candidateOxygenRatings, in.Diagnostics)
+	copy(candidateCO2Ratings, in.Diagnostics)
+
+	for bitPlace := len(bits) - 1; bitPlace >= 0; bitPlace-- {
+		log.Printf("searching for ratings bit place: %d, freqency: %d...", bitPlace, bits[bitPlace])
+		oxygenBitValueToKeep := (bits[bitPlace] >= len(in.Diagnostics)/2)
+		co2BitValueToKeep := (bits[bitPlace] < len(in.Diagnostics)/2)
+		for i := len(candidateOxygenRatings) - 1; i >= 0; i-- {
+			d := candidateOxygenRatings[i]
+			pow := uint64(math.Pow(2, float64(bitPlace)))
+			bitIsOn := (d & pow) > 0
+			if bitIsOn != oxygenBitValueToKeep {
+				candidateOxygenRatings = append(candidateOxygenRatings[:i], candidateOxygenRatings[i+1:]...)
+			}
+		}
+		for i := len(candidateCO2Ratings) - 1; i >= 0; i-- {
+			d := candidateCO2Ratings[i]
+			pow := uint64(math.Pow(2, float64(bitPlace)))
+			bitIsOn := (d & pow) > 0
+			if bitIsOn != co2BitValueToKeep {
+				candidateCO2Ratings = append(candidateCO2Ratings[:i], candidateCO2Ratings[i+1:]...)
+			}
+		}
+		log.Printf("for bit %d, got %d oxygen ratings", bitPlace, len(candidateOxygenRatings))
+		log.Printf("for bit %d, got %d co2 ratings", bitPlace, len(candidateCO2Ratings))
+		if len(candidateCO2Ratings) == 1 && out.ScrubberRating == 0 {
+			out.ScrubberRating = candidateCO2Ratings[0]
+		}
+		if len(candidateOxygenRatings) == 1 && out.OxygenRating == 0 {
+			out.OxygenRating = candidateOxygenRatings[0]
+		}
+	}
+
+	out.PartTwoAnswer = int(out.OxygenRating * out.ScrubberRating)
+
 	return out, nil
 }
 
