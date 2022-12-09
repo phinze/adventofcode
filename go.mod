ecd63bf4e7a6af9be7401cfedd104578e6016fe7
Paul Hinze <phinze@phinze.com>
2022-12-09 11:54:12 -0600

    Add generator for scaffolding

--- go.mod
+++ go.mod
@@ -16,6 +16,7 @@ require (
 require (
 	github.com/cheekybits/genny v1.0.0 // indirect
 	github.com/fsnotify/fsnotify v1.5.4 // indirect
+	github.com/gammban/numtow v0.0.2 // indirect
 	github.com/go-task/slim-sprig v0.0.0-20210107165309-348f09dbbbc0 // indirect
 	github.com/hashicorp/errwrap v1.1.0 // indirect
 	github.com/hashicorp/go-multierror v1.1.1 // indirect
--- main.go
+++ main.go
@@ -1,4 +1,150 @@
 package main
 
-func main() {
+import (
+	"errors"
+	"fmt"
+	"io/fs"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+	"text/template"
+
+	"github.com/gammban/numtow"
+	"github.com/gammban/numtow/lang"
+	"tailscale.com/util/must"
+)
+
+type AdventDay struct {
+	Year    int
+	Day     int
+	DayWord string
 }
+
+func ToWord(i int) string {
+	return strings.Title(numtow.MustInt64(int64(i), lang.EN))
+}
+
+// Generates
+func main() {
+	if len(os.Args) != 3 {
+		log.Fatalf("expected 2 args got %d", len(os.Args)-1)
+	}
+	year := must.Get(strconv.Atoi(os.Args[1]))
+	day := must.Get(strconv.Atoi(os.Args[2]))
+
+	basePath := fmt.Sprintf("./year%d/day%02d", year, day)
+	goPath := fmt.Sprintf("%s.go", basePath)
+	testPath := fmt.Sprintf("%s_test.go", basePath)
+
+	ctx := &AdventDay{
+		Year:    year,
+		Day:     day,
+		DayWord: ToWord(day),
+	}
+
+	err := writeTemplate(goPath, goTemplate, ctx)
+	if err != nil {
+		log.Fatalf("error writing go template: %s", err)
+	}
+	log.Printf("write go file to %s", goPath)
+
+	err = writeTemplate(testPath, testTemplate, ctx)
+	if err != nil {
+		log.Fatalf("error writing test template: %s", err)
+	}
+	log.Printf("write test file to %s", testPath)
+}
+
+func writeTemplate(path string, tmpl string, ctx *AdventDay) error {
+	_, err := os.Stat(path)
+	if !errors.Is(err, fs.ErrNotExist) {
+		return fmt.Errorf("file already exists: %s", path)
+	}
+
+	t := template.Must(template.New("file").Parse(tmpl))
+	f, err := os.Create(path)
+	if err != nil {
+		return fmt.Errorf("file create err: %s", err)
+	}
+	defer f.Close()
+	return t.Execute(f, ctx)
+}
+
+const goTemplate = `
+package year{{ .Year }}
+
+import (
+	"bufio"
+	"log"
+	"strings"
+)
+
+type Day{{ .DayWord }}Input struct {
+}
+type Day{{ .DayWord }}Output struct {
+}
+
+func parseDay{{ .DayWord }}(rawInput string) (*Day{{ .DayWord }}Input, error) {
+	scanner := bufio.NewScanner(strings.NewReader(rawInput))
+	in := &Day{{ .DayWord }}Input{}
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		log.Printf("line: %s", line)
+	}
+
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+
+	return in, nil
+}
+
+func solveDay{{ .DayWord }}(in *Day{{ .DayWord }}Input) (*Day{{ .DayWord }}Output, error) {
+	out := &Day{{ .DayWord }}Output{}
+
+	// part one
+
+	// part two
+
+	return out, nil
+}
+
+func Day{{ .DayWord }}(rawInput string) (*Day{{ .DayWord }}Output, error) {
+	in, err := parseDay{{ .DayWord }}(rawInput)
+	if err != nil {
+		return nil, err
+	}
+
+	out, err := solveDay{{ .DayWord }}(in)
+	if err != nil {
+		return nil, err
+	}
+
+	return out, nil
+}
+`
+
+const testTemplate = `
+package year{{ .Year }}
+
+import (
+	"testing"
+
+	"github.com/davecgh/go-spew/spew"
+	"github.com/phinze/adventofcode/aoc"
+)
+
+func TestDay{{ .DayWord }}(t *testing.T) {
+	in, err := aoc.FetchInput({{ .Year }}, {{ .Day }})
+	if err != nil {
+		t.Fatalf("err: %s", err)
+	}
+	out, err := Day{{ .DayWord }}(in)
+	if err != nil {
+		t.Fatalf("err: %s", err)
+	}
+	t.Logf("out: %s", spew.Sdump(out))
+}
+`
